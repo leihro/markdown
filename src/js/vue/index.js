902ab35c4a0f216177cbac39ee78908f1d8dd825
Lei Wang <wang@solutiondrive.de>
2016-03-26 17:45:46 +0100

    add cheatsheet function in Vue methods

--- src/js/vue/index.js
+++ src/js/vue/index.js
@@ -1,9 +1,29 @@
 new Vue({
-  el: '#my-editor',
-  data: {
-    input: '# Just a simple md editor\n\n[cheatsheet](http://md.captainpanda.de/cheatsheet.html) if you needed'
-  },
-  filters: {
-    marked: marked
-  }
+	el: '#my-editor',
+	data: {
+		input: '# Just a simple md editor\n\ncheatsheet below if you need it',
+		cheatsheet: {
+			'header': '# this is a header',
+			'list': '- this is a list'
+		}
+	},
+	filters: {
+		marked: marked
+	},
+	methods: {
+		addCheatSheet: function () {
+  			var key,
+  				cheatSheet = '';
+	  		for(key in this.cheatsheet) {
+	  			if(this.cheatsheet.hasOwnProperty(key)) {
+	  				cheatSheet += '\n' + this.cheatsheet[key];
+	  			}
+	  		}
+
+	  		return cheatSheet;
+	  	}
+  	},
+  	created () {
+  		this.input += this.addCheatSheet();
+  	}
 })
