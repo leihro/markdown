846836f3ed524dbdc187995ee1a7b7f147a1b8e8
Lei Wang <wang@solutiondrive.de>
2016-03-26 14:36:39 +0100

    add favicon and context change

--- src/js/vue/index.js
+++ src/js/vue/index.js
@@ -1,7 +1,7 @@
 new Vue({
   el: '#my-editor',
   data: {
-    input: '# Just a simple md my-Editor\n\n[cheatsheet](http://md.captainpanda.de/cheatsheet.html) if you needed'
+    input: '# Just a simple md editor\n\n[cheatsheet](http://md.captainpanda.de/cheatsheet.html) if you needed'
   },
   filters: {
     marked: marked
